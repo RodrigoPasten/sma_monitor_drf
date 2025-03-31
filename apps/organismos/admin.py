7d6598585940c2ab043705b1f16250ccc1f5e3c8
fjarar <39278638+fjarar@users.noreply.github.com>
2025-03-31 13:45:39 -0300

    feat / added organismos endpoint
    was added CRUD for organismos

--- apps/organismos/admin.py
+++ apps/organismos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Organismo
 
 # Register your models here.
+@admin.register(Organismo)
+class OrganismoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'sigla')
+    search_fields = ('nombre', 'sigla')
+    list_filter = ('activo',)
--- apps/organismos/models.py
+++ apps/organismos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Organismo(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    sigla = models.CharField(max_length=20, unique=True)
+    activo = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.nombre
